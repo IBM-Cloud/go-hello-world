7e0039d9eb07c10b1bab7fe955bdbc01ff45c6b2
Hanuvara <80243030+Hanuvara@users.noreply.github.com>
2023-03-02 20:07:42 +0400

    fix prod revert

--- custom_apps/main.go
+++ custom_apps/main.go
@@ -11,5 +11,5 @@ func main() {
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+    fmt.Fprintf(w, "Hello from prod, %s!", r.URL.Path[1:])
 }
